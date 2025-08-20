7475900e8d4299893ec952772ab68af022bb3ff8
Mara Masic <maramasic@berkeley.edu>
2025-08-20 13:54:58 -0700

    added docstrings

--- newton.py
+++ newton.py
@@ -1,35 +1,38 @@
 max_itter = 100
 h = 1e-5
 
-
 def function(x):
-    return x**2
+    return(x**2)
 
+#Derivatives
 
-# Derivatives
-
-
-def first_derivative(f, x, h=1e-5):
-    df = (f(x + h) - f(x)) / h
+def first_derivative(f, x, h = 1e-5):  #Calculate first derivative
+    '''Calculate first derivative using finite differences'''
+    
+    df = (f(x+h) - f(x))/h
     return df
 
-
-def second_derivative(f, x, h=1e-5):
+def second_derivative(f, x, h = 1e-5):  #Calculate second derivative
+    '''Calculate  second derivative using finite differences'''
+    
     df1 = first_derivative(f, x + h, h)
     df0 = first_derivative(f, x, h)
-    ddf = (df1 - df0) / h
+    ddf = (df1 - df0)/h
     return ddf
 
+#Newton's Method
 
-# Newton's Method
+def newtons_method(x_0, f, tol = 1e-7, max_iter = 100):
+    '''Newton's method Function'''
 
-
-def newtons_method(x_0, f, tol=1e-7, max_iter=100):
     x = x_0
-    df = first_derivative(f, x, h=1e-5)
-    ddf = second_derivative(f, x, h=1e-5)
-
+    df = first_derivative(f, x, h = 1e-5 )
+    ddf = second_derivative(f, x, h = 1e-5)
+    
     for i in range(max_itter):
         if (x - h) > 0.1:
-            x_1 = x - (df / ddf)
+            x_1 = x - (df/ddf)
         return x_1
+
+
+
