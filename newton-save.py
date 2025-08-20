ee78305935d5daf63605ad7c0f714eb4525471f9
Mara Masic <maramasic@berkeley.edu>
2025-08-20 13:39:48 -0700

    Reformatted newton.py

---
+++ newton-save.py
@@ -0,0 +1,32 @@
+max_itter = 100
+h = 1e-5
+
+def function(x):
+    return(x**2)
+
+#Derivatives
+
+def first_derivative(f, x, h = 1e-5):
+    df = (f(x+h) - f(x))/h
+    return df
+
+def second_derivative(f, x, h = 1e-5):
+    df1 = first_derivative(f, x + h, h)
+    df0 = first_derivative(f, x, h)
+    ddf = (df1 - df0)/h
+    return ddf
+
+#Newton's Method
+
+def newtons_method(x_0, f, tol = 1e-7, max_iter = 100):
+    x = x_0
+    df = first_derivative(f, x, h = 1e-5 )
+    ddf = second_derivative(f, x, h = 1e-5)
+    
+    for i in range(max_itter):
+        if (x - h) > 0.1:
+            x_1 = x - (df/ddf)
+        return x_1
+
+
+
--- newton.py
+++ newton.py
@@ -1,26 +1,35 @@
+max_itter = 100
+h = 1e-5
+
+
 def function(x):
-    return(x**2)
+    return x**2
 
-#Derivatives
 
-def first_derivative(f, x, h = 1e-5):
-    df = (f(x+h) - f(x))/h
+# Derivatives
+
+
+def first_derivative(f, x, h=1e-5):
+    df = (f(x + h) - f(x)) / h
     return df
 
-def second_derivative(f, x, h = 1e-5):
+
+def second_derivative(f, x, h=1e-5):
     df1 = first_derivative(f, x + h, h)
     df0 = first_derivative(f, x, h)
-    ddf = (df1 - df0)/h
+    ddf = (df1 - df0) / h
     return ddf
 
 
-#Newton's Method
+# Newton's Method
 
-def newtons_method(x_0, f, tol = 1e-7, max_iter = 100):
+
+def newtons_method(x_0, f, tol=1e-7, max_iter=100):
     x = x_0
-    for i in range(max_itter)
-    df = first_derivative(f, x_0, h = 1e-5 ))
-    ddf = second_derivative(f, x_0, h = 1e-5)
-    for (x_1 - h) > 0.1:
-        x_1 = x_0 - (df/ddf)
-    return x_1
+    df = first_derivative(f, x, h=1e-5)
+    ddf = second_derivative(f, x, h=1e-5)
+
+    for i in range(max_itter):
+        if (x - h) > 0.1:
+            x_1 = x - (df / ddf)
+        return x_1
